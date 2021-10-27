e7de23cfede78d0dbeff4c0b43d501036877b02e
Kristian Dam Pedersen <kristian.dam.pedersen@gmail.com>
2021-10-27 13:03:29 +0100

    import_nations tilføjer til liste

--- elements/import_data.py
+++ elements/import_data.py
@@ -2,10 +2,15 @@
 # Importering af data
 nations = open('/Users/kristiandampedersen/Documents/ip_eksamen/eksamensopgave_2/data/nations')
 
+
+# Definitioner af globale variable
+team_list = []
+
+
+# Importer nationer til en liste.
 def import_nations():
+    """ Læser nations.txt linje for linje og adder linjen til team_list listen """
     Lines = nations.readlines()
     for line in Lines:
         print('Line: {}'.format(line.strip()))
-
-import_nations()
-
+        team_list.append(line.strip())
--- tests.py
+++ tests.py
@@ -1,4 +1,8 @@
 # Det her dokument er for tests.
+#import '../elements/import_data' as impd
 
 def test_import_nations():
     pass
+    Lines = impd.nations.readlines()
+    for line in Lines:
+        print('Line: {}'.format(line.strip()))
