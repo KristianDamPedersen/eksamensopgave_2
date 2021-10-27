4aa24e82f2cc0c5871b227a05fef93c455381de8
Kristian Dam Pedersen <kristian.dam.pedersen@gmail.com>
2021-10-27 13:46:26 +0100

    Made import_matches read files correctly

--- elements/import_data.py
+++ elements/import_data.py
@@ -1,15 +1,21 @@
 # Importering af biblioteker
 from uuid import uuid4 # Helps generate unique ids
-# Importering af data
+# Importing data
 nations = open('/Users/kristiandampedersen/Documents/ip_eksamen/eksamensopgave_2/data/nations')
 
 
-# Definitioner af globale variable
+# Defining global variables
 team_list = []
 round_dict = {
     'round_id': [],
     'round' : [],
     'path' : []
+}
+
+match_dict = {
+    'match_text': [],
+    'match_id':[],
+    'round_id':[],
 }
 
 
@@ -23,11 +29,28 @@ def import_nations():
 
 
 # Constructing a list of rounds and paths
-def add_round (round, path_str):
+def import_rounds (round, path_str):
     """ This functions add a given round to the round_dict and generates a unique id for it. """
     round_dict['round'].append(round)
     round_dict['path'].append(path_str)
     round_dict['round_id'].append(uuid4())
 
+import_rounds('round1', '/Users/kristiandampedersen/Documents/ip_eksamen/eksamensopgave_2/data/round1')
+import_rounds('round2', '/Users/kristiandampedersen/Documents/ip_eksamen/eksamensopgave_2/data/round2')
+import_rounds('round3', '/Users/kristiandampedersen/Documents/ip_eksamen/eksamensopgave_2/data/round3')
+
+
+# Importing the matches
+def import_matches():
+    """ This function will look at all the files in the round_dict one by one and record the matches. """
+    for round in round_dict['path']:
+        round_matches = open(round)
+        matches = round_matches.readlines()
+        for line in matches:
+            print(line)
+        print(' ')
+
 
         
+
+import_matches()
