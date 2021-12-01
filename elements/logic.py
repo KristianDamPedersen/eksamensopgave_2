be4301e62313488c4a620e0d4f2914771e09132d
Kristian Dam Pedersen <kristian.dam.pedersen@gmail.com>
2021-12-01 11:34:18 +0100

    Made logic into a class, added customizability

--- elements/logic.py
+++ elements/logic.py
@@ -3,113 +3,114 @@
 from numpy import NaN
 import pandas as pd
 
+class logic:
+    points_win = 3
+    points_draw = 1
+    points_loss = 0
 
-points_win = 3
-points_draw = 1
-points_loss = 0
-
-def identify_winners():
-    """ Function that returns the winners of each unique match """
-    # Imports the data
-    from eksamensopgave_2.elements.import_data import imports as imp
-    df = imp.construct_match_df()
-    
-    # Appends the winner and losers to the dataframe
-    df['winner'] = NaN
-    df['loser'] = NaN
-    df['draw'] = 0
-    count = 0
-    for i in range(len(df)):
-        df_row = df.iloc[count]
-        if df_row['points_team_1'] > df_row['points_team_2']:
-            df.loc[df.index[count], 'winner'] = df.loc[df.index[count], 'team1']
-            df.loc[df.index[count], 'loser'] = df.loc[df.index[count], 'team2'] 
-        elif df_row['points_team_2'] > df_row['points_team_1']:
-            df.loc[df.index[count], 'winner'] = df.loc[df.index[count], 'team2']
-            df.loc[df.index[count], 'loser'] = df.loc[df.index[count], 'team1']
-        else:
-            df.loc[df.index[count], 'draw'] = 1
-        count += 1
-
-    # Returns the dataframe
-    return df
-
-def team_points():
-    """ This function calculates the number of points and goals and outputs a dataframe """
-    # import the data
-    df = identify_winners()
-
-    # Create a list of all unique team names
-    t1_names = list(df['team1'].unique())
-    t2_names = list(df['team2'].unique())
-    team_names = t1_names + t2_names
-    unique_names = []
-    for i in team_names:
-        # check if exists in unique_list or not
-        if i not in unique_names:
-            unique_names.append(i)
-    
-    # Create a df containing team names and points from wins
-    point_dict = {
-        'team': unique_names, 
-        'no. matches': [],
-        'no. wins': [],
-        'no. draws': [],
-        'no. losses': [],
-        'points_wins': [], 
-        'points_draws': [], 
-        'points_losses': [], 
-        'total_scores': []
-        }
-    
-    for i in point_dict['team']:
-
-        ## Retrieving the correct dataframe
-        data = df.copy()
-        data_t1 = data[data['team1'].isin([i])]
-        data_t2 = data[data['team2'].isin([i])]
-        data = data_t1.append(data_t2)
-
-        ## Calculate points from wins
-        win_df = data[data['winner'] == i]
-        point_dict['points_wins'].append(len(win_df)*points_win)
-
-        ## Calculate points from draws
-        draw_df = data[data['draw'] == 1]
-        point_dict['points_draws'].append(len(draw_df)*points_draw)
+    def identify_winners():
+        """ Function that returns the winners of each unique match """
+        # Imports the data
+        from eksamensopgave_2.elements.import_data import imports as imp
+        df = imp.construct_match_df()
         
-        ## Calculate points from losses
-        loss_df = data[data['loser'] == i]
-        point_dict['points_losses'].append(len(loss_df)*points_loss)
+        # Appends the winner and losers to the dataframe
+        df['winner'] = NaN
+        df['loser'] = NaN
+        df['draw'] = 0
+        count = 0
+        for i in range(len(df)):
+            df_row = df.iloc[count]
+            if df_row['points_team_1'] > df_row['points_team_2']:
+                df.loc[df.index[count], 'winner'] = df.loc[df.index[count], 'team1']
+                df.loc[df.index[count], 'loser'] = df.loc[df.index[count], 'team2'] 
+            elif df_row['points_team_2'] > df_row['points_team_1']:
+                df.loc[df.index[count], 'winner'] = df.loc[df.index[count], 'team2']
+                df.loc[df.index[count], 'loser'] = df.loc[df.index[count], 'team1']
+            else:
+                df.loc[df.index[count], 'draw'] = 1
+            count += 1
 
-        ## Calculate nr. of scores
-        scores_t1 = data[data['team1'] == i]
-        scores_t1 = scores_t1['points_team_1'].sum()
-        scores_t2 = data[data['team2'] == i]
-        scores_t2 = scores_t2['points_team_1'].sum()
-        scores = scores_t1 + scores_t2
-        point_dict['total_scores'].append(scores)
+        # Returns the dataframe
+        return df
 
-        ## Calculate no. matches
-        point_dict['no. matches'].append(len(data))
+    def team_points(win_points, draw_points, loss_points):
+        """ This function calculates the number of points and goals and outputs a dataframe """
+        # import the data
+        df = logic.identify_winners()
 
-        ## Calculate no. wins
-        point_dict['no. wins'].append(len(win_df))
+        # Create a list of all unique team names
+        t1_names = list(df['team1'].unique())
+        t2_names = list(df['team2'].unique())
+        team_names = t1_names + t2_names
+        unique_names = []
+        for i in team_names:
+            # check if exists in unique_list or not
+            if i not in unique_names:
+                unique_names.append(i)
+        
+        # Create a df containing team names and points from wins
+        point_dict = {
+            'team': unique_names, 
+            'no. matches': [],
+            'no. wins': [],
+            'no. draws': [],
+            'no. losses': [],
+            'points_wins': [], 
+            'points_draws': [], 
+            'points_losses': [], 
+            'total_scores': []
+            }
+        
+        for i in point_dict['team']:
 
-        ## Calculate no. draws
-        point_dict['no. draws'].append(len(draw_df))
+            ## Retrieving the correct dataframe
+            data = df.copy()
+            data_t1 = data[data['team1'].isin([i])]
+            data_t2 = data[data['team2'].isin([i])]
+            data = data_t1.append(data_t2)
 
-        ## Calculate no. losses
-        point_dict['no. losses'].append(len(loss_df))
+            ## Calculate points from wins
+            win_df = data[data['winner'] == i]
+            point_dict['points_wins'].append(len(win_df)*win_points)
 
-    score_df = pd.DataFrame(point_dict)
-    score_df['total_points'] = score_df['points_wins'] + score_df['points_draws'] + score_df['points_losses']
+            ## Calculate points from draws
+            draw_df = data[data['draw'] == 1]
+            point_dict['points_draws'].append(len(draw_df)*draw_points)
             
-    # Return
-    return score_df
+            ## Calculate points from losses
+            loss_df = data[data['loser'] == i]
+            point_dict['points_losses'].append(len(loss_df)*loss_points)
 
-def construct_rankings():
-    """ This function constructs the dataframe used for rankings with the ability to sort. """
-    pass
+            ## Calculate nr. of scores
+            scores_t1 = data[data['team1'] == i]
+            scores_t1 = scores_t1['points_team_1'].sum()
+            scores_t2 = data[data['team2'] == i]
+            scores_t2 = scores_t2['points_team_1'].sum()
+            scores = scores_t1 + scores_t2
+            point_dict['total_scores'].append(scores)
 
-team_points()
+            ## Calculate no. matches
+            point_dict['no. matches'].append(len(data))
+
+            ## Calculate no. wins
+            point_dict['no. wins'].append(len(win_df))
+
+            ## Calculate no. draws
+            point_dict['no. draws'].append(len(draw_df))
+
+            ## Calculate no. losses
+            point_dict['no. losses'].append(len(loss_df))
+
+        score_df = pd.DataFrame(point_dict)
+        score_df['total_points'] = score_df['points_wins'] + score_df['points_draws'] + score_df['points_losses']
+                
+        # Return
+        return score_df
+
+    def construct_rankings(win_points, draw_points, loss_points):
+        """ This function constructs the dataframe used for rankings with the ability to sort. """
+        df = logic.team_points(win_points, draw_points, loss_points)
+        df = df[['team', 'no. matches', 'no. wins', 'no. losses', 'no. draws', 'total_points', 'total_scores']]
+        return df
+logic.construct_rankings(logic.points_win, logic.points_draw, logic.points_loss)
